0ef5f325e0f34006d4c0c63251b38c4aa578b0d6
Cruz Zapil <victorzapil202030851@cunoc.edu.gt>
2024-02-24 23:31:44 -0600

    agregando actualizaciones

---
+++ src/main/java/com/logisticasBC/biblioteca/frontEnd/VentanaPrincipal.java
@@ -0,0 +1,17 @@
+package com.logisticasBC.biblioteca.frontEnd;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class VentanaPrincipal extends JFrame {
+
+    private static  JPanel panelCentral;
+    private static  JPanel panelMenu;
+
+    public VentanaPrincipal (){
+     
+    }
+
+    
+
+}
