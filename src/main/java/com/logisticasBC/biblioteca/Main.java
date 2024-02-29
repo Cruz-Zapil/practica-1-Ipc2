18739b947f78b58c5bcc96843ab7e2a447d8d34c
Cruz Zapil <victorzapil202030851@cunoc.edu.gt>
2024-02-29 02:53:51 -0600

    limpiando el area de trabajo antes de subir una actualizacion antes mensionada

--- src/main/java/com/logisticasBC/biblioteca/Main.java
+++ src/main/java/com/logisticasBC/biblioteca/Main.java
@@ -1,18 +1,9 @@
 package com.logisticasBC.biblioteca;
 
-<<<<<<< HEAD
+
 import com.logisticasBC.biblioteca.frontEnd.ventanaPrincipal.VentanaPrincipal;
-=======
-import com.logisticasBC.biblioteca.frontEnd.VentanaPrincipal;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
->>>>>>> main
 
-import java.time.LocalDate;
 
-import java.time.LocalDate;
-
-import java.time.LocalDate;
 
 public class Main  {
 
